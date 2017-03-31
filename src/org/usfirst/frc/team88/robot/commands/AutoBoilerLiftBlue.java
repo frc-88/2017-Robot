629d3b7b417bd39a8b2cf7a4950c5016313c0d92
MrNick <navtges@gmail.com>
2017-03-31 10:11:53 -0400

    Clean up AutoBoilerLiftBlue

--- src/org/usfirst/frc/team88/robot/commands/AutoBoilerLiftBlue.java
+++ src/org/usfirst/frc/team88/robot/commands/AutoBoilerLiftBlue.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team88.robot.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -9,26 +7,27 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutoBoilerLiftBlue extends CommandGroup {
 
-    public AutoBoilerLiftBlue() {
-		DriverStation ds = DriverStation.getInstance();
-		Preferences prefs = Preferences.getInstance();
-		
+	public AutoBoilerLiftBlue() {
 		addSequential(new DriveZeroYaw());
 		addParallel(new ShooterSetHood(.42));
-    	addSequential(new DriveDistance(-6));
-//    	addSequential(new DriveDistanceAndCurve(-6.8, 0.405, 2.95));
-		addSequential(new DriveRotateToAngle(60));
-    	addSequential(new Delay(0.3));
-		addSequential(new AutoDeliverGear(this));
-    	
-    	addSequential(new DriveRotateToBoiler());
-    	addSequential(new ShooterStartFlywheel());
-    	addSequential(new Delay(0.3));
-    	addSequential(new ShooterStartAgitatorAndFeeder());
-    	addSequential(new Delay(4.0));
+		addSequential(new DriveDistance(-6));
+		addSequential(new DriveScanForGear(90));
+		addSequential(new Delay(0.1));
 
-    	addSequential(new ShooterStopAll());
+		addSequential(new AutoDeliverGear(this));
+
+		addSequential(new DriveToBoiler());
+		addSequential(new Delay(0.1));
+		addSequential(new DriveRotateToBoiler());
+		addSequential(new Delay(0.1));
+
+		addSequential(new ShooterStartFlywheel());
+		addSequential(new Delay(0.3));
+		addSequential(new ShooterStartAgitatorAndFeeder());
+		addSequential(new Delay(4.0));
+
+		addSequential(new ShooterStopAll());
 		addSequential(new DriveRotateToAngle(0));
 		addSequential(new DriveDistance(-14.0));
-    }
+	}
 }
