e8a72ef951287702a601bb4304b4c2b842de935e
Allison Turner <hermione2259@gmail.com>
2017-04-04 18:58:12 -0400

    More auto adjustments

--- src/org/usfirst/frc/team88/robot/commands/AutoBoilerLiftBlue.java
+++ src/org/usfirst/frc/team88/robot/commands/AutoBoilerLiftBlue.java
@@ -10,14 +10,16 @@ public class AutoBoilerLiftBlue extends CommandGroup {
 	public AutoBoilerLiftBlue() {
 		addSequential(new DriveZeroYaw());
 		addParallel(new ShooterSetHood(.42));
-		addSequential(new DriveDistance(-6));
+    	addSequential(new DriveDistance(-6));
 		addSequential(new DriveScanForGear(90));
-		addSequential(new Delay(0.1));
-
-		addSequential(new AutoDeliverGear(this));
-
-		addSequential(new AutoShoot());
+    	addSequential(new Delay(0.1));
 		
+    	addSequential(new AutoDeliverGear(this));
+
+    	addSequential(new Delay(0.2));
+    	
+		addSequential(new AutoShoot());
+
 		addSequential(new DriveRotateToAngle(0));
 		addSequential(new DriveDistance(-14.0));
 	}
